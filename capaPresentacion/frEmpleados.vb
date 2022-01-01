717f05da3865b80f2f259863608cd933df6b3855
Administrador <Administrador@WIN-MD30UGF1LU3>
2022-01-01 19:08:43 +0100

    Primera vision del panel CRUD

--- capaPresentacion/frEmpleados.vb
+++ capaPresentacion/frEmpleados.vb
@@ -2,4 +2,12 @@
     Private Sub frEmpleados_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+
+    End Sub
 End Class
